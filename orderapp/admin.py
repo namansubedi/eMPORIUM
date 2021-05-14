a46853b21bf1304e1a6abf629f01ccc49f67d84c
Ishwarkc133 <ishwarkc133@gmail.com>
2021-05-14 17:04:42 +0545

    admin.py update

--- orderapp/admin.py
+++ orderapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(order)
+admin.site.register(order_item)
+admin.site.register(payment_details)
