a9aad13d87d8548903e67e6e3ac4e425e7f5a9e2
Naman Subedi <naman.subedi12@gmail.com>
2021-05-14 21:50:46 +0545

    added login/logout funnctionality

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from home import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('register', views.register, name="register"),
-    path('upload', views.upload, name="uplaod" )
+    path('upload', views.upload, name="uplaod" ),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render, redirect
 from home.models import profiles, products
 from django.contrib.auth.models import User, auth
@@ -85,3 +86,24 @@ def upload(request):
             print("invalid input")
     form = productsform()
     return render(request, "upload.html", {"form": form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Wrong Values Entered!')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
