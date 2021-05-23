c7b0c54891450d44a76077e31f1a61812477c98b
Kalyanbikramadhikari <kalyanad100@gmail.com>
2021-05-23 14:10:41 +0545

    profile page

--- empo/settings.py
+++ empo/settings.py
@@ -124,11 +124,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'ass')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
